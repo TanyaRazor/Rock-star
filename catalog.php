9f9e5c1294c9648d7528b8aa1b3fb611d70803ef
TanyaRazor <tarazdm@gmail.com>
2022-08-12 13:24:47 +0300

    create template header and footer

---
+++ catalog.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/style_catalog.css">
+    <link rel="stylesheet" href="/css/normalize.css">
+    <link rel="shortcut icon" href="/img/logo.png" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700;800&display=swap" rel="stylesheet">
+    <title>Караоке бар Rock-star</title>
+</head>
+
+<body>
+<?php include 'header.html';?>
+
+<main>
+  <div class="section-catalog">
+    <div class="container section-catalog__container">
+
+      <h2 class="header section-catalog__header">Каталог песен</h2>
+
+    </div>
+  </div>
+</main>
+
+<?php include 'footer.html';?>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/styli_index.css">
+    <link rel="stylesheet" href="/css/normalize.css">
+    <link rel="shortcut icon" href="/img/logo.png" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700;800&display=swap" rel="stylesheet">
+    <title>Караоке бар Rock-star</title>
+</head>
+
+<body>
+
+<?php include 'header.html';?>
+<main>
+  <div class="section-index">
+    <div class="container section-index__container">
+
+      <h2 class="header section-index__header">Главная</h2>
+
+    </div>
+  </div>
+</main>
+
+<?php include 'footer.html';?>
+
+</body>
+
+</html>
