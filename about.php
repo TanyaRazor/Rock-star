c7cd19a274b36d4702d09cca9b23dfe95e850cd9
TanyaRazor <tarazdm@gmail.com>
2022-08-15 15:36:26 +0300

    Add adaptive for desctop

--- about.php
+++ about.php
@@ -1,20 +1,7 @@
 <!DOCTYPE html>
 <html lang="ru">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="Cache-Control" content="no-cache">
-    <link rel="stylesheet" href="/css/style.css">
-    <link rel="stylesheet" href="/css/style_about.css">
-    <link rel="stylesheet" href="/css/normalize.css">
-    <link rel="shortcut icon" href="/img/logo.png" type="image/x-icon">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700;800&display=swap" rel="stylesheet">
-    <title>Караоке бар Rock-star</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="/js/header.js"></script>
-</head>
+<?php include 'head.html';?>
 
 <body>
 
