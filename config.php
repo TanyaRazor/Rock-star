9f9919fb59ec868f2fe3d11fdcd1dc0c5052cf24
TanyaRazor <tarazdm@gmail.com>
2022-09-30 22:28:54 +0300

    adding some fix

--- config.php
+++ config.php
@@ -1,8 +1,13 @@
 <?php
-$hostname = 'rockstar57.ru';
+// $hostname = 'rockstar57.ru';
+$hostname = '31.31.198.206';
 $username = 'u1611848_default';
 $password = 'MrX5umFtIC6yX68b5dSU';
 $dbname = 'u1611848_rockstar_orel';
+
+
+// $ip = gethostbyname('rockstar57.ru');
+// echo $ip;
 
 $con = mysqli_connect($hostname, $username, $password, $dbname);
 if (!$con) {
