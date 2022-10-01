37e9fbf9f45a9af652f84d2a6c93aa826e1c6793
TanyaRazor <tarazdm@gmail.com>
2022-10-01 13:30:28 +0300

    adding origin title

--- js/header.js
+++ js/header.js
@@ -1,6 +1,27 @@
 $(document).ready(function() {
 
     var main_id = $('main').attr('id');
+    var title_type = '';
+    switch (main_id) {
+        case "index":
+            title_type = 'Официальный сайт';
+            break;
+        case 'events':
+            title_type = 'Наши мероприятия';
+            break;
+        case 'catalog':
+            title_type = 'Каталог песен';
+            break;
+        case 'about':
+            title_type = 'О нас';
+            break;
+    }
+
+
+    $('title').text('Караоке бар Rock-staR - ' + title_type);
+    var title = $('title').text();
+    console.log(main_id);
+    console.log(title);
 
     $('a#' + main_id + "_tab").addClass('active-item');
     $('a#' + main_id + "_burger").addClass('active-item');
